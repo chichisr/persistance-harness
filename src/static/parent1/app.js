ad52c31062a1cf739bc9413670b69d24c60e74be
Chi-chi Wang <cwang@shoprunner.com>
2020-03-09 14:43:03 -0500

    [parents] Barebones logic for parent frames

--- src/static/parent1/app.js
+++ src/static/parent1/app.js
@@ -1,1 +1,7 @@
 console.log('[parent1] Script loaded');
+
+const $inlineIframe = sr_$.rmp.THEIframe(`http://${sr_$.rmp.THEURL}`);
+
+// Append inline iframe
+const $inline = document.getElementById('Inline');
+$inline.appendChild($inlineIframe);
--- src/static/parent2/app.js
+++ src/static/parent2/app.js
@@ -1,1 +1,7 @@
 console.log('[parent2] Script loaded');
+
+const $inlineIframe = sr_$.rmp.THEIframe(`http://${sr_$.rmp.THEURL}`);
+
+// Append inline iframe
+const $inline = document.getElementById('Inline');
+$inline.appendChild($inlineIframe);
