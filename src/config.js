a6e0e9ea98394a787999c1fa22e7ac36211e8572
Elliott Fodi <efodi@shoprunner.com>
2020-03-16 17:39:16 -0400

    safari poc using 3 different domains

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
-    parent1: 'foo.com',
-    parent2: 'bar.com',
-    child: 'baz.com',
+    parent1: 'retailer_111.com',
+    parent2: 'retailer_222.com',
+    child: 'spufflesss.com',
 };
 
 module.exports = config;
